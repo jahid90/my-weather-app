b788f46f5942dc4c2498cbd073af399c6bd9d833
Jahid <s.jahid.90@gmail.com>
2019-08-03 20:22:16 +0530

    [service] configure app to use static assets and handlebars templates

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
+// core modules
+const path = require('path')
+// npm modules
 const express = require('express');
+const hbs = require('hbs');
 
+// Create the app
 const app = express();
 
+// Setup app configurations
+const port = process.env.PORT || 3000;
+const staticResourcesPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Configure the app
+app.use(express.static(staticResourcesPath));
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup the root route
 app.get('', (req, res) => {
-    res.send('Hello World!');
+    res.render('index');
 });
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000.')
+// Start the server
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}.`)
 });
